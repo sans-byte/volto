01573a8db277b42de6a287ca21d15de4fb9a6025
wesleybl <wesleybl@gmail.com>
2023-02-08 14:58:36 -0300

    Show related items

--- src/config/index.js
+++ src/config/index.js
@@ -152,6 +152,7 @@ let config = {
     serverConfig,
     storeExtenders: [],
     showTags: true,
+    showRelatedItems: true,
     controlpanels: [],
     controlPanelsIcons,
     filterControlPanels,
